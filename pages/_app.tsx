197eca7ce73d84383324df3c481a3bcb21968263
Tormod Haugene <th@variant.no>
2021-11-24 10:23:39 +0100

    Add variant profile styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,6 @@
 import type { AppProps } from 'next/app';
 import { FC } from 'react';
 import { SiteLayout } from '@components/site-layout/SiteLayout';
-import '@styles/theme.css';
 import '@styles/global.css';
 
 const VariantdagApp: FC<AppProps> = ({ Component, pageProps }) => {
