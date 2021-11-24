0feee9258590e836c3e33fbc20f74a9fd1ccdc18
Tormod Haugene <th@variant.no>
2021-11-24 09:43:24 +0100

    Add semis to prettier rule

--- pages/typed-page.tsx
+++ pages/typed-page.tsx
@@ -1,11 +1,11 @@
 type Typo = {
-  test: string
-}
+  test: string;
+};
 
 export default function TypedPage() {
   const test: Typo = {
     test: 'I have type',
-  }
+  };
 
-  return <div>I am a page, and {test.test}!</div>
+  return <div>I am a page, and {test.test}!</div>;
 }
